2260112f246499e2037bb1eea73d5095cecf69f7
mdnajmul <mdnajmulovi@gmail.com>
2021-10-27 20:35:30 +0600

    top navbar section complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,81 @@
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faPhoneAlt, faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <section className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <img src="./health-logo.png" width="280" height="150" alt="" />
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 d-flex mt-5">
+            <span className="me-3">
+              <FontAwesomeIcon className="me-3 green-clr" icon={faEnvelope} />
+              <span className="green-clr" role="button">
+                {" "}
+                Email Us{" "}
+              </span>
+              <p className="fw-bold">homemedicare@health.com</p>
+            </span>
+            <span className="me-3">
+              <FontAwesomeIcon className="me-3 green-clr" icon={faPhoneAlt} />
+              <span className="green-clr"> Hotline </span>
+              <p className="fw-bold">09612 654321</p>
+            </span>
+            <span>
+              <FontAwesomeIcon className="me-3 green-clr" icon={faClock} />
+              <span className="green-clr"> Working Hour </span>
+              <p className="fw-bold">7AM - 11PM (Everyday)</p>
+            </span>
+          </div>
+          {/* <div className="col-lg-4 col-sm-6 mt-5">
+          <span>
+            
+          </span>
+        </div> */}
+        </div>
+      </section>
+
+      {/* nav bar */}
+      <Navbar sticky="top" bg="primary" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">HOMEMEDICARE & HOSPITAL</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">About Us</Nav.Link>
+              <Nav.Link href="#action3">Our Services</Nav.Link>
+              <Nav.Link href="#action3">Doctors</Nav.Link>
+              <Nav.Link href="#action3">Contact</Nav.Link>
+              <NavLink
+                className="text-decoration-none me-3 header-top-btn"
+                to="/login"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                className="text-decoration-none header-top-btn"
+                to="/register"
+              >
+                Sign Up
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
