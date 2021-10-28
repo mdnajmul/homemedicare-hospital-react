482560de206f5109c59f3e6c2ce391783d5c5acd
mdnajmul <mdnajmulovi@gmail.com>
2021-10-28 19:26:32 +0600

    set up firebase in this project

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3FNXCqyMycvQbXk0MgLAbxW4Ks5tn8zw",
+  authDomain: "homemedicare-hospital.firebaseapp.com",
+  projectId: "homemedicare-hospital",
+  storageBucket: "homemedicare-hospital.appspot.com",
+  messagingSenderId: "899870142672",
+  appId: "1:899870142672:web:5130400091616e029daed5",
+};
+
+export default firebaseConfig;
